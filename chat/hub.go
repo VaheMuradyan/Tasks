f36838b3a6bf7598692ce58fbbe63a8f861e3924
VaheMuradyan <vahenmuradyan02@outlook.com>
2024-10-17 16:18:13 +0400

    chat

---
+++ chat/hub.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bytes"
+	"html/template"
+	"log"
+	"sync"
+)
+
+type Hub struct {
+	sync.RWMutex
+
+	clients map[*Client]bool
+
+	broadcast  chan *Message
+	register   chan *Client
+	unregister chan *Client
+
+	messages []*Message
+}
+
+func NewHub() *Hub {
+	return &Hub{
+		clients:    map[*Client]bool{},
+		broadcast:  make(chan *Message),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+	}
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.Lock()
+			h.clients[client] = true
+			h.Unlock()
+
+			log.Printf("client registered %s", client.id)
+
+			for _, msg := range h.messages {
+				client.send <- getMessageTemplate(msg)
+			}
+		case client := <-h.unregister:
+			h.Lock()
+			if _, ok := h.clients[client]; ok {
+				close(client.send)
+				log.Printf("client unregistered %s", client.id)
+				delete(h.clients, client)
+			}
+			h.Unlock()
+		case msg := <-h.broadcast:
+			h.RLock()
+			h.messages = append(h.messages, msg)
+
+			for client := range h.clients {
+				select {
+				case client.send <- getMessageTemplate(msg):
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+			h.RUnlock()
+		}
+	}
+}
+
+func getMessageTemplate(msg *Message) []byte {
+	tmpl, err := template.ParseFiles("templates/message.html")
+	if err != nil {
+		log.Fatalf("template parsing: %s", err)
+	}
+
+	var renderedMessage bytes.Buffer
+	err = tmpl.Execute(&renderedMessage, msg)
+	if err != nil {
+		log.Fatalf("template execution: %s", err)
+	}
+
+	return renderedMessage.Bytes()
+}
